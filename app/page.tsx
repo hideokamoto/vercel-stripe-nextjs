e81744b8712b0455a0f7169e11f3282b63e98ac7
Hidetaka Okamoto <kokkoku214@gmail.com>
2023-09-07 14:49:27 +0900

    only remote patterns

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ export default async function Home() {
   const products = await stripe.products.list({
     expand: ['data.default_price']
   }).then(({data: items}) => items.filter(item => item.default_price && item.images.length > 0))
-  console.log(products)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
