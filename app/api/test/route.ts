8954ba303cd22fdde65f12b5938e1baeddcf14d6
Hidetaka Okamoto <kokkoku214@gmail.com>
2023-09-07 15:46:22 +0900

    test

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,10 +2,12 @@ import { cookies } from 'next/headers'
 import { NextResponse, NextRequest } from 'next/server'
 
 export const POST = async (request: NextRequest): Promise<NextResponse> => {
+    const cooky = cookies()
     const body = {
-        cookies: cookies().getAll(),
-        ga: cookies().get("_ga"),
-        isLoggedIn: cookies().get("isLoggedIn")
+        cookies: cooky.getAll(),
+        ga: cooky.get("_ga"),
+        isLoggedIn: cooky.get("isLoggedIn"),
+        test: cooky.get("cookie-test")
     }
     return NextResponse.json(body)
 }
