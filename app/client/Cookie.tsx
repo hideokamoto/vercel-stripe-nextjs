702fafdb29990f403aff5bfa42dc0b5b99330059
Hidetaka Okamoto <kokkoku214@gmail.com>
2023-09-07 15:32:50 +0900

    cookie

--- app/client/Cookie.tsx
+++ app/client/Cookie.tsx
@@ -6,9 +6,7 @@ export const ClientCookie: FC<{
 }> = ({apiBase}) => {
     const [cookies, setCookies] = useState('')
     useEffect(() => {
-        document.cookie = `cookie-test=${Math.random()}`
-    }, [])
-    useEffect(() => {
+        document.cookie = `cookie-test=${Math.random()};api-path=${apiBase}`
         fetch(`/api/test`, {
             method: 'POST',
             cache: 'no-cache',
